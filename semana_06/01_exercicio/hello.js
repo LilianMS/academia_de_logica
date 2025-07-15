a609910ffa0e3077d1c4aa22b8e488c37c1aa951
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-07-14 22:17:55 -0300

    :sparkles: first commit

---
+++ semana_06/01_exercicio/hello.js
@@ -0,0 +1,9 @@
+// hello.js
+
+let program = "Node.js";
+let version = process.version;
+// console.log("Hello, Node.js!");
+console.log(`Hello, ${program}!
+Version: ${version}
+Current directory: ${process.cwd()}`);
+console.log("")
