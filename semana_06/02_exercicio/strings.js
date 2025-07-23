377cd78bc9ba1cccf1d3782f5c6041b44618e0b1
Lilian Santos <100161978+LilianMS@users.noreply.github.com>
2025-07-22 22:26:49 -0300

    adds strings.js

---
+++ semana_06/02_exercicio/strings.js
@@ -0,0 +1,24 @@
+// 1 -
+const nome = "lilian";
+const lastName = "silva";
+
+const fullName = nome + " " + lastName;
+
+console.log("Nome completo:", fullName);
+
+// 2 -
+const boasvidas = `Olá, ${fullName}! Seja bem-vinda à sua primeira semana de estudos!`;
+
+console.log(boasvidas);
+
+// 3 -
+
+console.log("Comprimento do nome:", fullName.length);
+console.log("Comprimento do nome:", fullName.length);
+
+
+
+const nome_modficado = fullName.replace(nome, "Ly");
+
+console.log(nome_modficado);
+
